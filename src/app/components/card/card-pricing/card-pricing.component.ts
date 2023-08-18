7d45b31ec0b19b1d0bca758f411da5e118982871
Vinicius Moreira <vmarv@hotmail.com>
2023-08-18 16:21:21 -0300

    project finish

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-pricing.component.css']
 })
 export class CardPricingComponent {
+
+  @Input() gameType: string = "Digital PS4";
+  @Input() gamePrice: string = "R$ 399,90";
+
+  constructor() {
+  }
 
 }
